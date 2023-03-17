b2cacfbb71382502c6d666aaea1d1ca8a9c9e93c
Gourab-Ghosh <gourab200ghosh@gmail.com>
2023-03-17 05:36:28 +0530

    Implemented many things

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ class BoardGUI:
     RESOLUTION = 800
     OFFSET = roundint(0.04 * RESOLUTION)
     PIECE_SHIFT = (0 * np.array([1, 1]) * RESOLUTION).round().astype(int)
-    SHADOW_ALPHA = 0.5
+    SHADOW_ALPHA_PERCENT = 0.5
     CIRCLE_COLOR = CIRCLE_COLOR_CAPTURE = (0, 0, 0)
     CIRCLE_COLOR_ALPHA = 40
     CIRCLE_RADIUS = roundint((RESOLUTION - 2 * OFFSET) / 64)
@@ -148,12 +148,23 @@ class BoardGUI:
         self.event_handler = EventHandler(self)
         self.screen = pygame.display.set_mode((self.RESOLUTION, self.RESOLUTION))
         self.piece_symbols = "pnbrqkPNBRQK"
-        self.pieces_blit = {piece: self.render_object(chess.Piece.from_symbol(piece), (self.RESOLUTION - 2 * self.OFFSET) / 8) for piece in self.piece_symbols}
         self.selected_piece = None
         self.popped_moves = []
         self.arrows = set()
         self.highlight_squares_dict = {}
+        self.generate_blits()
+
+    def generate_blits(self):
+        self.pieces_blit = {piece: self.render_object(chess.Piece.from_symbol(piece), (self.RESOLUTION - 2 * self.OFFSET) / 8) for piece in self.piece_symbols}
         self.update_board_blit()
+
+        self.circle = pygame.Surface((2*self.CIRCLE_RADIUS, 2*self.CIRCLE_RADIUS), pygame.SRCALPHA)
+        pygame.draw.circle(self.circle, self.CIRCLE_COLOR_CAPTURE, (self.CIRCLE_RADIUS, self.CIRCLE_RADIUS), self.CIRCLE_RADIUS)
+        self.circle.set_alpha(self.CIRCLE_COLOR_ALPHA)
+
+        self.circle_capture = pygame.Surface((2*self.CIRCLE_RADIUS_CAPTURE, 2*self.CIRCLE_RADIUS_CAPTURE), pygame.SRCALPHA)
+        pygame.draw.circle(self.circle_capture, self.CIRCLE_COLOR, (self.CIRCLE_RADIUS_CAPTURE, self.CIRCLE_RADIUS_CAPTURE), self.CIRCLE_RADIUS_CAPTURE, self.CIRCLE_THICKNESS_CAPTURE)
+        self.circle_capture.set_alpha(self.CIRCLE_COLOR_ALPHA)
 
     def render_object(self, obj, resolution: int, **kwargs):
         resolution = roundint(resolution)
@@ -256,23 +267,16 @@ class BoardGUI:
                     circle_coordinate = (x + (self.RESOLUTION - 2 * self.OFFSET) / 16, y + (self.RESOLUTION - 2 * self.OFFSET) / 16)
                     for move in self.board.generate_legal_moves(chess.BB_SQUARES[self.dragging_piece_square]):
                         if square == move.to_square and move.promotion in [None, chess.QUEEN]:
-                            radius = self.CIRCLE_RADIUS_CAPTURE if self.board.is_capture(move) else self.CIRCLE_RADIUS
-                            color = self.CIRCLE_COLOR_CAPTURE if self.board.is_capture(move) else self.CIRCLE_COLOR
-                            thickness = self.CIRCLE_THICKNESS_CAPTURE if self.board.is_capture(move) else 0
-                            alpha = self.CIRCLE_COLOR_ALPHA
-                            circle = pygame.Surface((2*radius, 2*radius), pygame.SRCALPHA)
-                            pygame.draw.circle(circle, color, (radius, radius), radius, thickness)
-                            circle.set_alpha(alpha)
+                            circle = self.circle_capture if self.board.is_capture(move) else self.circle
                             self.screen.blit(circle, circle.get_rect(center = circle_coordinate))
 
         if dragging_piece_blit:
             self.screen.blit(*dragging_piece_blit)
 
     def get_promotion_piece_type(self):
-        font = pygame.font.SysFont(None, 24)
-        screen_width, screen_height = self.screen.get_size()
-        dialog_width = 200
-        dialog_height = 100
+        screen_width = screen_height = self.RESOLUTION
+        dialog_width = roundint(self.RESOLUTION / 4)
+        dialog_height = roundint(self.RESOLUTION / 4)
         dialog_left = (screen_width - dialog_width) // 2
         dialog_top = (screen_height - dialog_height) // 2
         dialog_rect = pygame.Rect(dialog_left, dialog_top, dialog_width, dialog_height)
@@ -280,7 +284,7 @@ class BoardGUI:
         # Render board and make shadow
         self.render_board()
         shadow = pygame.Surface((self.RESOLUTION, self.RESOLUTION))
-        shadow.set_alpha(roundint(self.SHADOW_ALPHA * 255))
+        shadow.set_alpha(roundint(self.SHADOW_ALPHA_PERCENT * 255))
         self.screen.blit(shadow, (0, 0))
 
         # Draw the dialog box
@@ -293,27 +297,27 @@ class BoardGUI:
         rook_rect = pygame.Rect(dialog_left + dialog_width // 2 + 5, dialog_top + 10, dialog_width // 2 - 15, dialog_height // 2 - 15)
         queen_rect = pygame.Rect(dialog_left + dialog_width // 2 + 5, dialog_top + dialog_height // 2 + 5, dialog_width // 2 - 15, dialog_height // 2 - 15)
 
-        knight_button = font.render("Knight", True, (0, 0, 0))
-        bishop_button = font.render("Bishop", True, (0, 0, 0))
-        rook_button = font.render("Rook", True, (0, 0, 0))
-        queen_button = font.render("Queen", True, (0, 0, 0))
+        knight_button = self.pieces_blit["n" if self.board.turn else "N"]
+        bishop_button = self.pieces_blit["b" if self.board.turn else "B"]
+        rook_button = self.pieces_blit["r" if self.board.turn else "R"]
+        queen_button = self.pieces_blit["q" if self.board.turn else "Q"]
 
         pygame.draw.rect(self.screen, (192, 192, 192), knight_rect)
         pygame.draw.rect(self.screen, (192, 192, 192), bishop_rect)
         pygame.draw.rect(self.screen, (192, 192, 192), rook_rect)
         pygame.draw.rect(self.screen, (192, 192, 192), queen_rect)
 
-        self.screen.blit(knight_button, knight_rect.move(10, 5))
-        self.screen.blit(bishop_button, bishop_rect.move(10, 5))
-        self.screen.blit(rook_button, rook_rect.move(10, 5))
-        self.screen.blit(queen_button, queen_rect.move(10, 5))
+        self.screen.blit(knight_button, knight_button.get_rect(center = knight_rect.center))
+        self.screen.blit(bishop_button, bishop_button.get_rect(center = bishop_rect.center))
+        self.screen.blit(rook_button, rook_button.get_rect(center = rook_rect.center))
+        self.screen.blit(queen_button, queen_button.get_rect(center = queen_rect.center))
 
         pygame.display.update()
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
+                    self.running = False
                     return None
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     return None
