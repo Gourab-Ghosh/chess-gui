6c30de196d5d0eeea31dc10312915da93cc9dcce
Gourab-Ghosh <gourab200ghosh@gmail.com>
2024-07-21 16:27:26 +0530

    Now it works for any UCI chess engine

--- test.py
+++ test.py
@@ -3,27 +3,30 @@ import sys
 import subprocess
 from chess_gui import ChessGUI
 
-try:
-    from stockfish import Stockfish
-except:
-    pass
-else:
-    class Stockfish(Stockfish):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.board_fens = []
+# try:
+#     from stockfish import Stockfish
+# except:
+#     pass
+# else:
+#     class Stockfish(Stockfish):
+#         def __init__(self, *args, **kwargs):
+#             super().__init__(*args, **kwargs)
+#             self.board_fens = []
 
-        def make_move(self, move_uci):
-            self.board_fens.append(self.get_fen_position())
-            self.make_moves_from_current_position([move_uci])
+#         def make_move(self, move_uci):
+#             self.board_fens.append(self.get_fen_position())
+#             self.make_moves_from_current_position([move_uci])
 
-        def undo_move(self):
-            self.set_fen_position(self.board_fens.pop())
+#         def set_fen(self, fen_position: str, send_ucinewgame_token: bool = True) -> None:
+#             return super().set_fen_position(fen_position, send_ucinewgame_token)
+
+#         def undo_move(self):
+#             self.set_fen_position(self.board_fens.pop())
 
 class Timecat:
-    def __init__(self, path: str = "timecat", depth: int = 10):
+    def __init__(self, path: str = "timecat"):
         self._path = os.path.abspath(path)
-        self.depth = depth
+
         self._timecat = subprocess.Popen(
             self._path,
             universal_newlines=True,
@@ -32,7 +35,6 @@ class Timecat:
             stderr=subprocess.STDOUT,
         )
         self._has_quit_command_been_sent = False
-        self._put("set color false")
         self.disable_info = True
 
     def _read_line(self) -> str:
@@ -53,20 +55,18 @@ class Timecat:
 
     def make_move(self, move_uci):
         self._put("push uci {}".format(move_uci))
-        print(self._read_line())
 
     def undo_move(self):
         self._put("pop")
-        print(self._read_line())
 
     def get_best_move(self):
-        self._put("go depth {}".format(self.depth))
+        self._put("go movetime 1000")
         while True:
             line = self._read_line()
             if not (self.disable_info and (line.lower().startswith("info") or line.lower().startswith("pv"))):
                 print(line)
-            if line.startswith("Best Move"):
-                best_move = line.split()[-1]
+            if line.startswith("bestmove"):
+                best_move = line.strip().split()[1]
                 break
         return best_move
     
@@ -80,16 +80,15 @@ class Timecat:
         self.quit()
         del self
 
-d = 10
-timecat = Timecat(depth = d)
-# timecat.disable_info = False
-# stockfish = Stockfish("./stockfish", depth = d - 1)
+timecat = Timecat("dumbcat")
+timecat.disable_info = False
+# stockfish = Stockfish("stockfish", depth = d - 1)
 
 board_gui = ChessGUI()
 # board_gui.add_white_engine(timecat)
 # board_gui.add_black_engine(stockfish)
 board_gui.add_black_engine(timecat)
-board_gui.set_fen("8/8/8/5K2/2k5/2p5/7R/8 w - - 0 1")
+# board_gui.set_fen("1b2R3/8/P4k2/7p/8/7P/6K1/8 b - - 7 54")
 
 # board_gui.run()
 board_gui.play()
